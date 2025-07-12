cfa78244232a3ecdb5f2f5c733e784102364d795
Bistrina Sarkar <bistrinasarkar2654@gmail.com>
2025-07-13 00:21:14 +0530

    add

---
+++ script.js
@@ -0,0 +1,8 @@
+const items = document.querySelectorAll('.menu-item');
+
+items.forEach(item => {
+  item.addEventListener('click', () => {
+    items.forEach(i => i.classList.remove('active'));
+    item.classList.add('active');
+  });
+});
