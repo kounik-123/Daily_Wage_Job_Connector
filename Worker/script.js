173ba56b26341e2387d1aad347f6d4a81f323f13
kounik-123 <125635852+kounik-123@users.noreply.github.com>
2025-07-28 04:06:24 +0530

    all changes

--- Worker/script.js
+++
@@ -1,8 +0,0 @@
-const items = document.querySelectorAll('.menu-item');
-
-items.forEach(item => {
-  item.addEventListener('click', () => {
-    items.forEach(i => i.classList.remove('active'));
-    item.classList.add('active');
-  });
-});
